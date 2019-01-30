5c66055e6c1a04d1904f46dae4e560de24b1dcd9
Kaiohschumis <Kaiohschumis@gmail.com>
2019-01-30 16:01:46 -0200

    Classe domain

--- src/main/java/com/kaio/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/kaio/cursomc/resources/CategoriaResource.java
@@ -1,16 +1,26 @@
 package com.kaio.cursomc.resources;
 
+import com.kaio.cursomc.domain.Categoria;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 
-    @RequestMapping(method = RequestMethod.GET)
-    public String listar (){
-        return "REST está funcionando";
+    @RequestMapping(method=RequestMethod.GET)
+    public List<Categoria> listar(){
+
+        Categoria cat1 = new Categoria(1, "Informatica");
+        Categoria cat2 = new Categoria(1, "Escritório");
+
+        List<Categoria> lista = new ArrayList<>();
+        lista.add(cat1);
+        lista.add(cat2);
+        return lista;
     }
 
 }
