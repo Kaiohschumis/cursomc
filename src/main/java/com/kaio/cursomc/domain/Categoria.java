86478077297bd879ce38975fa5bb31bd77799235
Kaiohschumis <Kaiohschumis@gmail.com>
2019-02-01 14:50:13 -0200

    Endpoint/Verificar

--- src/main/java/com/kaio/cursomc/domain/Categoria.java
+++ src/main/java/com/kaio/cursomc/domain/Categoria.java
@@ -1,10 +1,11 @@
 package com.kaio.cursomc.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
+import javax.persistence.*;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Entity
@@ -15,6 +16,10 @@ public class Categoria implements Serializable {
     @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Integer id;
     private String nome;
+
+    @JsonManagedReference
+    @ManyToMany(mappedBy="categorias")
+    private List<Produto> produtos = new ArrayList<>();
 
     public Categoria (){
     }
@@ -38,6 +43,14 @@ public class Categoria implements Serializable {
 
     public void setId(Integer id) {
         this.id = id;
+    }
+
+    public List<Produto> getProdutos() {
+        return produtos;
+    }
+
+    public void setProdutos(List<Produto> produtos) {
+        this.produtos = produtos;
     }
 
     @Override
--- src/main/java/com/kaio/cursomc/services/CategoriaService.java
+++ src/main/java/com/kaio/cursomc/services/CategoriaService.java
@@ -2,6 +2,7 @@ package com.kaio.cursomc.services;
 
 import com.kaio.cursomc.domain.Categoria;
 import com.kaio.cursomc.repositories.CategoriaRepository;
+import com.kaio.cursomc.services_exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,8 +14,11 @@ public class CategoriaService {
     private CategoriaRepository repos;
 
     public Categoria buscar(Integer id){
-       Optional<Categoria> obj = repos.findById(id);
-       return obj.orElse(null);
+       Optional<Categoria> obj = repos.findById(id); //consulta repositoryy
+       if (obj == null) {
+            throw new ObjectNotFoundException("Objeto não encontrado! ID: " + id + ", Tipo: " + Categoria.class.getName());
+       }
+        return obj.orElse(null);
 
     }
 }
